3b2e986a9dc191886f9c73349ddf1703dc8fa584
Jerko <jerkokosic1@gmail.com>
2020-06-06 22:58:12 +0200

    modify

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 function OpenCon(){
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "1234";
-    $db = "cats";
+    $dbhost = "eu-cdbr-west-03.cleardb.net";
+    $dbuser = "b34287243877bc";
+    $dbpass = "db7ac085";
+    $db = "heroku_17af0e6b7b35bdc";
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
     
     return $conn;
